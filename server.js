edc04af1c18adec77462a335a9cc8b47f010adb0
foreveryoung777 <52020745+foreveryoung777@users.noreply.github.com>
2019-07-08 11:17:51 +0800

    Modify server.js
    Modify server.js

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ let server= http.createServer(app).listen(7000,function(){
 
 let data = {appId: "", nodeId: "", host: ""}
 
-app.use('/static', express.static(__dirname+'/dist/static'))
-
 let appIdIndex = -1
 let nodeIdIndex = -1
 let hostIndex = -1
@@ -42,18 +40,39 @@ data.nodeId = allArguments[nodeIdIndex]
 data.host = allArguments[hostIndex]
 console.log("Get process parameters id: " + data.id + ", nodeId: " + data.nodeId+ " host: " + data.host)
 
-app.get('/index.html', function(req, res, next){
-	console.log("Read index html.")
-	fs.readFile('dist/index.html', 'utf-8', function(err, str){
-		if (err) {
-			throw err
-		}
-		str = str.replace('{{appId}}', data.id)
-		str = str.replace('{{nodeId}}', data.nodeId)
-		str = str.replace('{{host}}', data.host)
-		console.log("Pass process parameters complete.")
-		res.send(str)
-	})
+fs.readFile('dist/index.html', 'utf-8', function(err, str){
+	console.log("Modify index.html.");
+	if (err) {
+		throw err
+	}
+	str = str.replace('{{appId}}', data.id)
+	str = str.replace('{{nodeId}}', data.nodeId)
+	str = str.replace('{{host}}', data.host)
+	console.log("Pass process parameters complete.")
+	fs.writeFile('dist/index.html', str, function(err){
+		if(err) throw err;
+		console.log('Writerite inddex.html complete.');
+	});
 })
 
+//app.get('/index.html', function(req, res, next){
+	//console.log("Read index html.")
+	//fs.readFile('index.html', 'utf-8', function(err, str){
+		//if (err) {
+			//throw err
+		//}
+		//str = str.replace('{{appId}}', data.id)
+		//str = str.replace('{{nodeId}}', data.nodeId)
+		//str = str.replace('{{host}}', data.host)
+		//console.log("Pass process parameters complete.")
+		//res.send(str)
+	//})
+//})
+
+app.use('/', express.static(__dirname+'/dist/'))
+
+app.get('/', function(req, res){
+	res.redirect('index.html');
+	res.end()
+})
 	
