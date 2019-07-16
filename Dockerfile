72f0b762b8039ac7e8f881f6b3867224e2789797
ryanaltair_quicklane <al_tair@live.com>
2019-07-16 17:05:38 +0800

    cleans

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM node:10.15
-WORKDIR /home/app/
-COPY package.json package-lock.json ./
-RUN npm install
-COPY server.js ./
-CMD /bin/bash
--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source config
-
-image=registry.cn-shanghai.aliyuncs.com/shuzhi/${IMAGE_NAME} :${VERSION}
-docker build -f Dockerfile -t ${image}  .
-docker push  ${image}
