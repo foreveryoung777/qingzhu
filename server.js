4e90919539c3bb5f26a9fadff8b9467794b1994a
zongjie <zongjie.wxt@xuelangyun.com>
2020-06-08 13:49:14 +0800

    parameters

--- server.js
+++ server.js
@@ -1,58 +1,65 @@
-const express = require('express')
-const http = require('http')
-const fs = require('fs')
-const { parameter } = require('suanpan_node_sdk').sp
-let app = express()
+const express = require("express");
+const http = require("http");
+const fs = require("fs");
+let app = express();
 
-let server= http.createServer(app).listen(7000,function(){
-    console.log(`listen at 7000`)
-})
+let server = http.createServer(app).listen(7000, function () {
+  console.log(`listen at 7000`);
+});
 
-let data = {appId: "", nodeId: "", host: ""}
+let appIdIndex = -1;
+let nodeIdIndex = -1;
+let hostIndex = -1;
+let backendUrlIndex = -1;
 
-let appIdIndex = -1
-let nodeIdIndex = -1
-let hostIndex = -1
-
-let allArguments = process.argv
+let allArguments = process.argv;
 
 allArguments.forEach((val, index) => {
-    if (val == "--stream-app-id") {
-		appIdIndex = index + 1
-	}
-	if (val == "--stream-node-id") {
-		nodeIdIndex = index + 1
-	}
-	if (val == "--stream-host") {
-		hostIndex = index + 1
-	}
-})
-data.id = allArguments[appIdIndex]
-data.nodeId = allArguments[nodeIdIndex]
-data.host = allArguments[hostIndex]
+  if (val == "--stream-app-id") {
+    appIdIndex = index + 1;
+  }
+  if (val == "--stream-node-id") {
+    nodeIdIndex = index + 1;
+  }
+  if (val == "--stream-host") {
+    hostIndex = index + 1;
+  }
+  if (val == "--backendUrl") {
+    backendUrlIndex = index + 1;
+  }
+});
 
-console.log(`Get process parameters id: ${data.id}, nodeId: ${data.nodeId}, host: ${data.host}, parameters: ${parameter}`)
+const trim = (str) => {
+	return str.slice(str.indexOf("'") + 1, str.lastIndexOf("'"));
+}
 
-fs.readFile('dist/index.html', 'utf-8', function(err, str){
-	console.log("Modify index.html.");
-	if (err) {
-		throw err
-	}
-	str = str.replace('{{appId}}', data.id)
-	str = str.replace('{{nodeId}}', data.nodeId)
-	str = str.replace('{{host}}', data.host)
-	str = str.replace('{{parameters}}', parameter)
-	console.log("Pass process parameters complete.")
-	fs.writeFile('dist/index.html', str, function(err){
-		if(err) throw err;
-		console.log('Writerite inddex.html complete.');
-	});
-})
+const data = {
+  appId: allArguments[appIdIndex],
+  nodeId: allArguments[nodeIdIndex],
+  url: trim(allArguments[hostIndex]),
+  parameters: { backendUrl: trim(allArguments[backendUrlIndex]) },
+};
 
-app.use('/', express.static(__dirname+'/dist/'))
+console.log(
+  `Get process parameters appId: ${data.appId}, nodeId: ${data.nodeId}, url: ${data.url}, parameters: ${JSON.stringify(data.parameters)}`
+);
 
-app.get('/', function(req, res){
-	res.redirect('index.html');
-	res.end()
-})
-	
+fs.readFile("dist/index.html", "utf-8", function (err, str) {
+  console.log("Modify index.html.");
+  if (err) {
+    throw err;
+  }
+  str = str.replace("{{appConfig}}", JSON.stringify(data));
+  console.log("Pass process parameters complete.");
+  fs.writeFile("dist/index.html", str, function (err) {
+    if (err) throw err;
+    console.log("Writerite inddex.html complete.");
+  });
+});
+
+app.use("/", express.static(__dirname + "/dist/"));
+
+app.get("/", function (req, res) {
+  res.redirect("index.html");
+  res.end();
+});
